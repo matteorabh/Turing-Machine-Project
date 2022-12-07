54870b9b43b8f5abc971ba73c411d52a14b50ac4
matteorabh <101938282+matteorabh@users.noreply.github.com>
2022-12-07 11:58:01 +0100

    question2 pull up (shalla c'est ce que veut le prof)

--- question1.py
+++ question1.py
@@ -1,14 +1,14 @@
-class MT (object):
+class MT(object):
 
-    def __init__(self,etat,etat_bande,tete_lecture,fichier):
+    def __init__(self,etat,etat_bande,tete_lecture):
         self.etat = etat
         self.etat_bande = etat_bande
         self.tete_lecture = tete_lecture
-        self.fichier = fichier
 
-
-def initialisation(mot=input("Veuillez sélectionner un mot d'entrée"),fichier="MT_Donnee.txt"):
-    return MT('I',mot,0,fichier)
+def initialisation(mot,fichier="MT_Donnee.txt"):
+    l_mot = [lettre for lettre in mot]
+    return MT('I',l_mot,0)
 
 
 # Doit-on faire des unittest ?
+# Question 2 : machine de turing = fichier ou = class MT
