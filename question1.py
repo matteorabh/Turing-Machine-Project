7276d068b334180e02b767c0c110b5580148f9b7
matteorabh <101938282+matteorabh@users.noreply.github.com>
2022-12-13 14:46:11 +0100

    modif supp_n

--- question1.py
+++ question1.py
@@ -5,13 +5,12 @@ class MT(object):
         self.etat_bande = etat_bande
         self.tete_lecture = tete_lecture
         self.nb_bande = 0
-        transitions = {}
+        self.transitions = False
 
-    def preparation(fichier):
+    def preparation(self,fichier):
         with open("fichier.txt") as f:
             lines = supp_n(f.readlines())
-
-        
+        self.transitions = {lines[i] : lines[i+1] for i in range(0,len(lines),2)}
             
         return
 
@@ -28,5 +27,5 @@ def supp_n(lines):
     Fonction qui supprime les \n d'une liste
     (Utile après le stockage du fichier)
     """
-    return [lines[i][:lines[i].find('\n')] for i in range(len(lines))]
+    return [lines[i][:lines[i].find('\n')] for i in range(len(lines)) if lines[i] != '\n']
 
