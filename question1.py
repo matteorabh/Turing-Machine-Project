42565733df1513e50a2a86fd11909f8c9302c950
matteorabh <101938282+matteorabh@users.noreply.github.com>
2022-12-14 15:51:55 +0100

    tete lecture dico

--- question1.py
+++ question1.py
@@ -5,15 +5,16 @@ class MT(object):
     def __init__(self,etat,etat_bande,tete_lecture):
         self.etat = etat
         self.etat_bande = etat_bande
-        self.tete_lecture = tete_lecture
+        self.tete_lecture = {}
         self.nb_bande = 0
         self.transitions = False
 
-    def preparation(self,fichier):
+    def preparation_initial(self,fichier):
         with open(fichier) as f:
             lines = outil.supp_n(f.readlines())
         self.transitions = {lines[i] : lines[i+1] for i in range(0,len(lines),2)}
         self.nb_bande = lines[0].split(',')-1
+        self.tete_lecture = {i:0 for i in range()}
         #Vérifications qu'il n'y a pas d'erreur dans le fichier
         outil.verif_determinisme(fichier,self.nb_bande,self.transitions)
         outil.verif_etat_initial_final(fichier)
@@ -24,8 +25,8 @@ class MT(object):
         print("--------------------------------")
 
 def initialisation(mot,fichier):
-    M1 = MT('I',{1:mot},0)
-    M1.preparation(fichier)
+    M1 = MT('I',{1:mot},)
+    M1.preparation_initial(fichier)
     return M1
 
 M1 = initialisation('kayak','MT_Donnee.txt')
