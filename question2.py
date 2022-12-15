811174769dc4e985b40dcd363edc79ab8b1cb9f6
matteorabh <101938282+matteorabh@users.noreply.github.com>
2022-12-15 10:17:47 +0100

    modif question2

--- question2.py
+++ question2.py
@@ -1,6 +1,15 @@
 import question1
 
 
+def allonge_bande(liste,binaire):
+    if binaire == 0:
+        l = ['_'] 
+        for elem in liste:
+            l.append(elem)
+        return l 
+    else:
+        return liste.append('_')
+
 def pas(MT):  
     if MT.transitions != False:
         nb_bande = MT.nb_bande
@@ -17,8 +26,9 @@ def pas(MT):
             MT.etat_bande[i][MT.tete_lecture[i]] = ligne_2[i]
             if ligne_2[i+nb_bande] == '>':
                 MT.tete_lecture[i] += 1
+                allonge_bande(MT.etat_bande[i],1)
             elif ligne_2[i+nb_bande] == '<':
-                MT.tete_lecture[i] -= 1
+                MT.etat_bande[i] = allonge_bande(MT.etat_bande[i],0)
     else:
         print("Vous n'avez pas initialisé votre machine de Turing!")
         exit()
